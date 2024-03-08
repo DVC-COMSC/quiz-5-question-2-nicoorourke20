e17b91e98a63eb50f63c3c080de6536f4d2a7461
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-07 21:05:23 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include    <iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
    int N, M;
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
-   /* Complete your code */
-  
+   
+   if (N <= 0 || N >= 10 || M < 1 || M > 10) {
+        cerr << "Error: N must be between 1 and 9, and M must be between 1 and 10." << endl;
+        return 1;
+    }
 }
